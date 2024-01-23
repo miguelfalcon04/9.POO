aab18c122b013c053c1743960e14a40c6577b289
miguelfalcon04 <falconmiguel.2004@gmail.com>
2024-01-23 12:16:31 +0100

    commit

---
+++ POO/POO_En_Java/Eje03/Lagarto.java
@@ -0,0 +1,17 @@
+package POO.POO_En_Java.Eje03;
+
+public class Lagarto extends Animal{
+
+    public Lagarto(String s){
+        super(s);
+    }
+
+    public void repta(){
+        System.out.println("mira como me muevo");
+    }
+
+    public void olfatea(){
+        System.out.println("olfateo con nariz");
+    }
+
+}
