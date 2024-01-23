4f861fe9b6c14bfd9af758f76d2eabe1a7b5f056
miguelfalcon04 <falconmiguel.2004@gmail.com>
2024-01-23 12:16:19 +0100

    commit

---
+++ POO/POO_En_Java/Eje03/Canario.java
@@ -0,0 +1,15 @@
+package POO.POO_En_Java.Eje03;
+
+public class Canario extends Ave {
+
+    public Canario(String s){
+        super(s);
+    }
+
+    public void canta(){
+        System.out.println("Soy el mejor cantante, lalalalala");
+    }
+
+    
+
+}
