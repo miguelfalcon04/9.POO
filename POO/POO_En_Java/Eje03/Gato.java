8014c3d647f59dd15090177c36422065d6d8d27d
miguelfalcon04 <falconmiguel.2004@gmail.com>
2024-01-23 12:16:24 +0100

    commit

---
+++ POO/POO_En_Java/Eje03/Gato.java
@@ -0,0 +1,17 @@
+package POO.POO_En_Java.Eje03;
+
+public class Gato extends Mamifero {
+
+    public Gato(String s){
+        super(s);
+    }
+
+    public void subeArbol(){
+        System.out.println("Mira como escalo con mis garras");
+    }
+
+    public void ronronea(){
+        System.out.println("grr grr");
+    }
+
+}
