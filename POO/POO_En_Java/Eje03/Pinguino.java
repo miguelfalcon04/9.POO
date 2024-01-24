ce93cb2b505fdfe9ce91f1b0e42f4d809ce07249
miguelfalcon04 <falconmiguel.2004@gmail.com>
2024-01-24 19:36:47 +0100

    commit

---
+++ POO/POO_En_Java/Eje03/Pinguino.java
@@ -0,0 +1,17 @@
+package POO.POO_En_Java.Eje03;
+
+public class Pinguino extends Ave {
+
+    public Pinguino(String s){
+        super(s);
+    }
+
+    public void deslizarse(){
+        System.out.println("Mira como me deslizo sobre mi panza");
+    }
+
+    public void vuela(){
+        System.out.println("Soy un pinguino no puedo volar");
+    }
+
+}
