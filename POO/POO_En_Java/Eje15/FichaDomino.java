9cdc68111bc9cfe65253ad576246e01bba137b8c
miguelfalcon04 <falconmiguel.2004@gmail.com>
2024-01-31 10:26:18 +0100

    commit

---
+++ POO/POO_En_Java/Eje15/FichaDomino.java
@@ -0,0 +1,55 @@
+package POO.POO_En_Java.Eje15;
+
+public class FichaDomino {
+
+    private int izquierda;
+    private int derecha;
+
+    public FichaDomino(){
+        this.izquierda = 0;
+        this. derecha = 0;
+    }
+
+    public FichaDomino(int izquierda, int derecha){
+        this.izquierda = izquierda;
+        this. derecha = derecha;
+    }
+
+    @Override
+    public String toString() {
+        return "["+this.izquierda+"|"+this.derecha+"]";
+    }
+
+    public int getDerecha() {
+        return derecha;
+    }
+
+    public void setDerecha(int derecha) {
+        this.derecha = derecha;
+    }
+
+    public int getIzquierda() {
+        return izquierda;
+    }
+
+    public void setIzquierda(int izquierda) {
+        this.izquierda = izquierda;
+    }
+
+    public String voltea(){
+        int aux = this.derecha;
+        this.derecha = this.izquierda;
+        this.izquierda = aux;
+
+        return toString();
+    }
+
+    public boolean encaja(FichaDomino f){
+        if(this.derecha==f.derecha || this.derecha == f.izquierda || this.izquierda == f.derecha || this.izquierda == f.izquierda){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
