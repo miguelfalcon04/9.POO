cefa8c3cc6682fa22df30222c40899a7d17dcafe
miguelfalcon04 <falconmiguel.2004@gmail.com>
2024-02-02 12:20:40 +0100

    commit

---
+++ POO/POO_En_Java/Eje17/Prueba.java
@@ -0,0 +1,14 @@
+package POO.POO_En_Java.Eje17;
+
+public class Prueba {
+    public static void main(String[] args) {
+        Piramide p = new Piramide(4);
+        Rectangulo r1 = new Rectangulo(4, 3);
+        Rectangulo r2 = new Rectangulo(6, 2);
+        System.out.println(p);
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println("Pirámides creadas: " + Piramide.getPiramidesCreadas());
+        System.out.println("Rectángulos creados: " + Rectangulo.getRectangulosCreados());
+    }
+}
