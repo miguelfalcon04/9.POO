edfbcb86030eb709c9dbacc4120d4588dcac6cce
miguelfalcon04 <falconmiguel.2004@gmail.com>
2024-02-07 10:34:09 +0100

    commit

---
+++ POO/ArraysDeObjetos/Eje03/MyUuid.java
@@ -0,0 +1,20 @@
+package POO.ArraysDeObjetos.Eje03;
+
+public class MyUuid {
+    public static String getUuid(int digits){
+        String ret = "";
+        final char A = 'A';
+        final char ZERO = '0';
+        for (int i = 1; i <= digits; i++) {
+            if(Math.random()>0.5){
+                //one letter
+                ret += (char)((int)A+(int)(Math.random()*26));
+            }else{
+                //one digit
+                ret += (char)(ZERO+(int)(Math.random()*10));
+            }
+        }
+
+        return ret;
+    }
+}
